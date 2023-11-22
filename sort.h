d6de4f6059f5d71545967d0af9b46325a7341bea
Pacific1990 <abet.eko@gmail.com>
2023-11-21 22:53:32 -0800

    commit 014

--- sort.h
+++ sort.h
@@ -1,23 +1,7 @@
-#ifndef _SORT_H_
-#define _SORT_H_
+#ifndef SORT_H
+#define SORT_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-
-#define UP 0
-#define DOWN 1
-
-/**
- * enum my_bool - Enumeration of Boolean values.
- * @false: Equals 0.
- * @true: Equals 1.
- */
-typedef enum my_bool
-{
-false = 0,
-true
-} my_bool;
 
 /**
  * struct listint_s - Doubly linked list node
@@ -43,9 +27,6 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
 
-#endif /* _SORT_H_ */
+
+#endif /* SORT_H */
